2dcd3249c766210be057f9e01d38593fe942c738
Verner Codoceo <vacodoceo@uc.cl>
2020-06-15 17:25:02 -0400

    Add rifas count and ranking commands

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
-require('dotenv').config()
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const axios = require("axios");
+require("dotenv").config();
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
-})
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on("message", async (msg) => {
+  if (msg.content.startsWith("yecta ")) {
+    const command = msg.content.replace("yecta ", "");
+    if (command === "ping") {
+      msg.reply("Pong!");
+    } else if (command === "rifas vendidas") {
+      const url = `${process.env.API_URL}?spreadsheetId=${process.env.SPREADSHEET_ID}`;
+      const res = await axios.get(url);
+      let count = 0;
+      res.data.sales.forEach((s) => (count += s.quantity));
+      msg.reply(count);
+    } else if (command === "rifas ranking") {
+      const url = `${process.env.API_URL}?spreadsheetId=${process.env.SPREADSHEET_ID}`;
+      const res = await axios.get(url);
+      const vendors = {};
+      res.data.sales.forEach((s) => {
+        if (!vendors[s.vendor]) vendors[s.vendor] = 0;
+        vendors[s.vendor] += s.quantity;
+      });
+
+      const rankingArray = Object.entries(vendors)
+        .sort((a, b) => b[1] - a[1]);
+
+      console.log(rankingArray)
+
+      let ranking = "\n**RANKING DE VENDEDORES:**"
+      rankingArray.slice(0, 10).forEach((r, i) => {
+        ranking += `\n${i+1}. ${r[0]} (${r[1]})` 
+      })
+
+      msg.reply(ranking);
+    } else {
+      msg.reply(`Comando inválido! Intenta con alguno de los siguientes comandos:
+        - yecta rifas vendidas
+        - yecta rifas ranking
+
+      Si te gustaría que existiese otro comando, háblale a <@285326725955715073>`);
+    }
   }
-})
+});
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
